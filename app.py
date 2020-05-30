f30ec14542531562e2d1081740929d77f3ddd030
Chad Gielow <Chad.Gielow@nike.com>
2020-05-30 11:02:39 -0700

    pushing API changes

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ def plays():
         NFL.play_type,
         NFL.total_home_score,
         NFL.total_away_score, 
-        NFL.team_id.all()
+        NFL.team_id.all())
+    
 
     session.close()
 
