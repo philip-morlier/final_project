ff3dc48a43f56f61146c4be3241364f4fbb8f46c
philip-morlier <58234998+philip-morlier@users.noreply.github.com>
2020-05-30 01:37:47 -0700

    updated flask file

--- myapp.py
+++ myapp.py
@@ -2,12 +2,16 @@ import flask
 import pickle
 import pandas as pd
 import os
-from transformation_methods import Transformer
+from transformation_methods import *
 
 
 with open(f'model/play_forest_v1.pkl', 'rb') as f:
     model = pickle.load(f)
 
+post_data=Playdata()
+
+# import pdb
+# pdb.set_trace()
 
 app = flask.Flask(__name__, template_folder='templates')
 @app.route('/')
@@ -15,25 +19,41 @@ app = flask.Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
+
     if flask.request.method == 'GET':
-        return(flask.render_template('main.html'))
+
+
+        play = flask.request.args.get('playtype','pass')
+        play_type_cat = Transformer().play_interpreter(play)
+        post_data.set_play_variable(play_type_cat)
+        # post_play=post_data.get_variable()
+
+        ydsnet = flask.request.args.get('ydsnet',5)
+        post_data.set_yrd_variable(ydsnet)
+
+
+
+        return(flask.render_template('pre_play.html'))
+
+
 
     if flask.request.method == 'POST':
+
+        # pdb.set_trace()
         
    
         period = flask.request.form['quarter']
         quarter = Transformer().quarter_interpreter(period)
         clock_time = flask.request.form['time']
-        play = flask.request.form['playtype']
         dn = flask.request.form['down']
         
         posteam = flask.request.form['posteam']
         ydstogo = flask.request.form['ydstogo']
         yardline_100 = flask.request.form['yardline'] 
-        play_type_cat = Transformer().play_interpreter(play)
+        play_type_cat = post_data.get_play_variable()
         down = Transformer().down_interpreter(dn)
         game_seconds_remaining = Transformer().time_converter(clock_time,quarter)
-        ydsnet = flask.request.form['ydsnet']
+        ydsnet = post_data.get_yrd_variable()
 
 
         input_variables = pd.DataFrame([[posteam,ydstogo,yardline_100,play_type_cat,down,
@@ -46,9 +66,7 @@ def main():
         prediction = Transformer().result_interpreter(output)
 
 
-        return flask.render_template('main.html',
-                                     result=prediction,
-                                     )
+        return flask.render_template('main_2.html',result=prediction)
 
 
 if __name__ == '__main__':
